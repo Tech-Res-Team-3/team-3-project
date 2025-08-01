4c2d8d46f1c2bbe176a361debe6dd9d68bba9d6c
alivsey87 <andre.livsey@gmail.com>
2025-08-01 14:08:26 -0400

    Frontend: Created Button component

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,11 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}", "./components/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        ruby: "#c41111"
+      }
+    },
   },
   plugins: [],
 }
