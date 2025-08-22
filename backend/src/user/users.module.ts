f513569ddaf6cafea24db14a6f5a905bceaa3e36
alivsey87 <andre.livsey@gmail.com>
2025-08-22 13:03:19 -0400

    Frontend: deleted import line from users module for backend

--- backend/src/user/users.module.ts
+++ backend/src/user/users.module.ts
@@ -1,7 +1,6 @@
 import { Module } from '@nestjs/common';
 import { UsersService } from './providers/users.service';
 import { UsersController } from './users.controller';
-import { Prisma } from 'generated/prisma';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { FirebaseService } from 'src/firebase/firebase.service';
 
