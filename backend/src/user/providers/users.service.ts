013eb75b7454a479a2be05d0ce8e2bd19016663a
cam96stanley <cam96stanley@gmail.com>
2025-08-23 02:56:31 -0400

    updated patch user to update user with currently logged in users uid instead of a param

--- backend/src/user/providers/users.service.ts
+++ backend/src/user/providers/users.service.ts
@@ -1,12 +1,12 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../../prisma/prisma.service';
 import { GetUsersParamDto } from '../dto/get-users-param.dto';
+import { PatchUserDto } from '../dto/patch-user.dto';
 
 @Injectable()
 export class UsersService {
   constructor(private prisma: PrismaService) {}
 
-  
   async upsertUser(data: {
     firebaseUid: string;
     email: string;
@@ -14,12 +14,12 @@ export class UsersService {
     role: 'GUEST' | 'ADMIN';
   }) {
     const [firstName, lastNameRaw] = data.displayName
-    ? data.displayName.split(' ')
-    : data.email
-    ? [data.email.split('@')[0], '']
-    : ['Unknown', 'User'];
+      ? data.displayName.split(' ')
+      : data.email
+        ? [data.email.split('@')[0], '']
+        : ['Unknown', 'User'];
     const lastName = lastNameRaw ?? '';
-    
+
     return this.prisma.user.upsert({
       where: { firebaseUid: data.firebaseUid },
       update: {
@@ -37,7 +37,7 @@ export class UsersService {
       },
     });
   }
-  
+
   async getUsers(
     getUsersParamDto: GetUsersParamDto,
     limit: number,
@@ -45,24 +45,24 @@ export class UsersService {
   ) {
     return this.prisma.user.findMany();
   }
-  
+
   async getUserById(firebaseUid: string) {
     return this.prisma.user.findUnique({ where: { firebaseUid } });
   }
-  
-  async updateUser (firebaseUid: string, data: any) {
+
+  async updateUser(firebaseUid: string, data: PatchUserDto) {
     const user = await this.prisma.user.findUnique({ where: { firebaseUid } });
-    
+
     if (!user) throw new Error('User not found');
     return this.prisma.user.update({
       where: { firebaseUid },
       data,
     });
   }
-  
+
   async promoteToHost(firebaseUid: string) {
     const user = await this.prisma.user.findUnique({ where: { firebaseUid } });
-    
+
     if (!user) throw new Error('User not found');
     const newRole = user.role === 'GUEST' ? 'HOST' : 'GUEST';
     return this.prisma.user.update({
@@ -70,8 +70,8 @@ export class UsersService {
       data: { role: newRole },
     });
   }
-  
+
   async deleteUser(firebaseUid: string) {
-    return this.prisma.user.delete({ where: { firebaseUid  } });
+    return this.prisma.user.delete({ where: { firebaseUid } });
   }
 }
